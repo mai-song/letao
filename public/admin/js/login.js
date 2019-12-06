a8284014514172e0274a49d39c7a99c749b0e408
mai-song <948732080@qq.com>
2019-12-06 20:57:10 +0800

    登录功能完成了

--- public/admin/js/login.js
+++ public/admin/js/login.js
@@ -1,7 +1,8 @@
 $(function(){
   // 登录功能
   // 1表单的校验
-  $('form').bootstrapValidator({
+  var $form=$('form')
+  $form.bootstrapValidator({
     fields:{
       username:{
         validators:{
@@ -12,6 +13,10 @@ $(function(){
             min: 3,
             max: 12,
             message: '用户名长度必须在3到12之间'
+          },
+          callback:{
+            message: '用户名不存在'
+           
           }
         
         }
@@ -25,6 +30,10 @@ $(function(){
             min: 6,
             max: 12,
             message: '密码长度必须在6到12之间'
+          },
+          callback:{
+            message: '密码错误'
+           
           }
         
         }
@@ -36,4 +45,38 @@ $(function(){
     validating: 'glyphicon glyphicon-refresh'
   },
   })
+  // 2表单重置功能
+  $('.btn-reset').on('click',function(){
+    $form.data('bootstrapValidator').resetForm(true);
+  })
+   // 给登录按钮注册电机事件
+  
+   $form.on('success.form.bv', function (e) {
+    e.preventDefault();
+    $.ajax({
+      type:'post',
+      url:'/employee/employeeLogin',
+      data:$form.serialize(),
+      beforeSend:function(){
+        NProgress.start()
+      },
+      success:function(info){
+        console.log(info);
+        
+        if(info.success){
+          location.href="index.html"
+        }else if(info.error===1000){
+          $form.data('bootstrapValidator').updateStatus('username','INVALID','callback')
+        }else{
+          $form.data('bootstrapValidator').updateStatus('password','INVALID','callback')
+          
+        }
+        
+      },
+      complete:function(){
+        NProgress.start()
+        
+      }
+    })
+});
 })
